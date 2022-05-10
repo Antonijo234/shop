7777e1d602645324309a96195abdfc900568aff5
Antonijo <antonijo.brezak@gmail.com>
2022-05-10 14:46:26 +0200

    now working XD

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_10_082857) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_10_123840) do
+  create_table "articles", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
